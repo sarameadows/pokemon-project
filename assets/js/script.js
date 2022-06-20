c03e439bb4a8fa11d73773fdc11f8d500dcd1e91
Sara <sameads9@gmail.com>
2022-06-19 19:32:44 -0500

    add script src to html and test pokemon search form button functionality

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+//variables for html elements
+var pokemonPhotoAreaEL = document.querySelector("#pokemon-photo-area");
+var pokemonNameDisplayEL = document.querySelector("#pokemon-name-display");
+var pokemonStatsDisplayEL = document.querySelector("#pokemon-stats-display");
+var pokemonInputEl = document.querySelector("#pokemon-name-select");
+var cityInputEl = document.querySelector("#city");
+var pokemonSearchButtonEl = document.querySelector("#pokemon-btn");
+var citySearchButtonEl = document.querySelector("#weather-btn");
+var weatherDisplayEl = document.querySelector("#weather-display");
+var pokemonSearchFormEl = document.querySelector("#pokemon-search");
+
+//get pokemon data based on input
+var getPokeData = function(pokemon) {
+    console.log(pokemon);
+};
+
+// send pokemon name from search bar to getPokeDatafunction
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    
+    //get value from input element
+    var selectedPokemon = pokemonInputEl.value.trim();
+
+    // if it is a real pokemon get the stats, otherwise display error
+    if(selectedPokemon) {
+        getPokeData(selectedPokemon);
+        pokemonInputEl.value = "";
+        //set to local storage
+        //store(selectedCity);
+    }
+};
+
+pokemonSearchFormEl.addEventListener("submit", formSubmitHandler);
